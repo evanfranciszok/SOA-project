2404b81fabb13f1eb350bd1a725b071a310353e2
Floris Vossebeld <f.r.vossebeld@student.utwente.nl>
2024-04-03 10:34:27 +0200

    Change hostname

--- meal-planning-ui/src/lib/data.ts
+++ meal-planning-ui/src/lib/data.ts
@@ -4,6 +4,6 @@
 import {UserProfile} from "@/types";
 
 export async function fetchProfileData(): Promise<UserProfile> {
-    const response = await fetch('http://localhost:8088/profiles/123');
+    const response = await fetch('http://user_profile:8080/profiles/123');
     return response.json();
 }
