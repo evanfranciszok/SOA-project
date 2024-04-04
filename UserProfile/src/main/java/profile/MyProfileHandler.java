4e92eff88fd81c49e6fcdd7fe1c004ac4ba33fff
Evan Franciszok <git@evan.mailtje.net>
2024-04-04 19:57:43 +0200

    include backup for the mongoDB

--- UserProfile/src/main/java/profile/MyProfileHandler.java
+++ UserProfile/src/main/java/profile/MyProfileHandler.java
@@ -10,7 +10,6 @@ import com.sun.net.httpserver.HttpHandler;
 
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
-import java.util.Iterator;
 
 public class MyProfileHandler implements HttpHandler {
 
@@ -21,25 +20,12 @@ public class MyProfileHandler implements HttpHandler {
     }
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        // Set response headers
-        exchange.getResponseHeaders().set("Content-Type", "text/plain");
-        exchange.sendResponseHeaders(200, 0);
-
-        StringBuilder str = new StringBuilder();
-
-        MongoCollection<Document> collection = preferenceCollection;
-        FindIterable<Document> iterDoc = collection.find();
-        Iterator it = iterDoc.iterator();
-        while (it.hasNext()) {
-            str.append(it.next());
-            // System.out.println(it.next());
-        }
-
-        // Prepare the response body
-
-        // Write the response body to the output stream
-        try (OutputStream os = exchange.getResponseBody()) {
-            os.write(str.toString().getBytes());
-        }
+        FindIterable<Document> allFoodItems = this.preferenceCollection.find().projection(new Document("_id", 0));
+        String jsonOutput = HTTPHelper.getJsonOutputFromIterableDocument(allFoodItems);
+        
+        exchange.getResponseHeaders().set("Content-Type", "application/json");
+        exchange.sendResponseHeaders(200, jsonOutput.length());
+        
+        HTTPHelper.outputJson(exchange.getResponseBody(), jsonOutput);
     }
 }
