f97b35bf08f7bfcd93f4bf0495db8eed7da0ea62
Evan Franciszok <git@evan.mailtje.net>
2024-04-06 13:18:20 +0200

    update pom.xml so we can run it without mvn package

--- UserProfile/src/main/java/profile/UserProfileService.java
+++ UserProfile/src/main/java/profile/UserProfileService.java
@@ -4,6 +4,7 @@ import com.sun.net.httpserver.HttpServer;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
 
 import java.io.IOException;
 
@@ -12,12 +13,16 @@ import java.net.InetSocketAddress;
 public class UserProfileService {
 
     public static void main(String[] args) throws IOException {
+        String uri = "mongodb://profile_database:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
         // String uri =
-        // "mongodb://profile_database:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
-        String uri = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
+        // "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
         MongoDatabase database;
         try {
             MongoClient mongoClient = MongoClients.create(uri);
+            MongoIterable<String> list = mongoClient.listDatabaseNames();
+            for (String name : list) {
+                System.out.println(name);
+            }
             database = mongoClient.getDatabase("profiles");
             HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
             server.createContext("/food", new MyFoodHandler(database.getCollection("foods")));
