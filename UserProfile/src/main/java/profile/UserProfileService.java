345a272a90bbb569b41ffe8c7d8486c11eb2e673
Evan Franciszok <git@evan.mailtje.net>
2024-04-10 13:47:35 +0200

    actually uploading dummy data in the database

--- UserProfile/src/main/java/profile/UserProfileService.java
+++ UserProfile/src/main/java/profile/UserProfileService.java
@@ -1,14 +1,21 @@
 package profile;
 
 import com.sun.net.httpserver.HttpServer;
+import com.google.gson.Gson;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import org.bson.Document;
 
 public class UserProfileService {
 
@@ -46,19 +53,30 @@ public class UserProfileService {
         for (String collectionName : collectionNames) {
             if (!database.listCollectionNames().into(new ArrayList<>()).contains(collectionName)) {
                 database.createCollection(collectionName);
-                // insertDataInTheCollection(database.getCollection(FOOD_COLLECTION_NAME), collectionName);
+                insertDataInTheCollection(database.getCollection(collectionName), collectionName);
             }
         }
         System.out.println("database connected and the correctly configured");
     }
 
-    // private static void insertDataInTheCollection(MongoCollection<Document> mongoCollection, String collectionName) {
-    //     try {
-    //         String jsonData = new String(Files
-    //                 .readAllBytes(Paths.get("src/main/resources/" + DATABASE_NAME + "." + collectionName + ".json")));
-    //     } catch (IOException e) {
-    //         // TODO Auto-generated catch block
-    //         e.printStackTrace();
-    //     }
-    // }
+    private static void insertDataInTheCollection(MongoCollection<Document> mongoCollection, String collectionName) {
+        try {
+            String jsonData = new String(Files
+                    .readAllBytes(Paths.get("src/main/resources/" + DATABASE_NAME + "." + collectionName + ".json")));
+
+            Gson gson = new Gson();
+            List<Map<String, Object>> jsonObjects = gson.fromJson(jsonData, ArrayList.class);
+
+            List<Document> documents = new ArrayList<>();
+            for (Map<String, Object> jsonObject : jsonObjects) {
+                jsonObject.remove("_id");
+                documents.add(new Document(jsonObject));
+            }
+            mongoCollection.insertMany(documents);
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
